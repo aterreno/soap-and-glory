f8b346417c11f5db3bc8180cab8a9a1b07327fa5
Antonio Terreno <antonio.terreno@gmail.com>
2014-11-27 17:14:39 +0000

    clustering in node.js is fun

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ var v1 = require('./routes/v1');
 var v2 = require('./routes/v2');
 var contentTypes = require('./middlewares/contentTypes');
 var bodyParser = require('./middlewares/bodyParser');
+var cluster = require('cluster');
 
-app.use(contentTypes());
-app.use(bodyParser());
-app.use('/v1', v1);
-app.use('/v2', v2);
+if (cluster.isMaster) {
+  var cpuCount = require('os').cpus().length;
 
-var server = app.listen(3000, function () {
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+} else {
+  app.use(contentTypes());
+  app.use(bodyParser());
+  app.use('/v1', v1);
+  app.use('/v2', v2);
 
-  var host = server.address().address
-  var port = server.address().port
+  var server = app.listen(3000, function () {
 
-  console.log('Server listening at http://%s:%s', host, port);
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Server listening at http://%s:%s - cluster.worker.id:%s', host, port, cluster.worker.id);
+  });
+}
+
+cluster.on('exit', function (worker) {
+  console.log('Worker ' + worker.id + ' died :(');
+  cluster.fork();
 });
